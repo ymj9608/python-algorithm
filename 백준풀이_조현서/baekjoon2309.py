7a44cdab8c41bbb547392591a48c7dfe023117ed
814andy <814andy@gmail.com>
2025-08-05 09:08:25 +0900

    주석 추가

--- 백준풀이_조현서/baekjoon2309.py
+++ 백준풀이_조현서/baekjoon2309.py
@@ -1,14 +1,19 @@
-list1 = [int(input()) for _ in range(9)]  # 일곱 난쟁이의 리스트를 설정
+# 아홉 명의 난쟁이 리스트를 설정
+list1 = [int(input()) for _ in range(9)] 
 
 total = sum(list1)
 
+# 첫 번째 가짜 난쟁이를 도출하기 위한 반복문
 for i in range(9):
+    # 두 번째 가짜 난쟁이를 도출하기 위한 반복문
     for j in range(i+1, 9):
         if total - (list1[i] + list1[j]) == 100:
-            false1, false2 = list1[i], list1[j]
+            false1, false2 = list1[i], list1[j]  # 가짜 난쟁이 2명 도출
 
+            # 가짜 난쟁이 2명을 제외한 난쟁이들을 리스트에 담음
             result = [l for l in list1 if l != list1[i] and l != list1[j]]
-
-            for r in sorted(result):
+            
+            # 리스트를 오름차순으로 정렬 후 출력
+            for r in sorted(result): 
                 print(r)
 
--- 백준풀이_조현서/baekjoon2920.py
+++ 백준풀이_조현서/baekjoon2920.py
@@ -1,9 +1,12 @@
-# 8개의 숫자가 배열로 주어짐
+# 8개의 숫자(음계)가 배열로 주어짐
 arr = list(map(int, input().split()))
 
+# 오름차순
 if arr == [1, 2, 3, 4, 5, 6, 7, 8]:
-    print("ascending") 
+    print("ascending")
+# 내림차순      
 elif arr == [8, 7, 6, 5, 4, 3, 2, 1]:
-    print("descending") 
+    print("descending")
+# 이 외     
 else:
     print("mixed")
