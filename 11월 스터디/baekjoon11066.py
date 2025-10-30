6f349fea29e2f8d292bd80b8ee778a43bff9448c
윤명진 <ymj9608@gmail.com>
2025-10-30 17:47:40 +0900

    수정

--- 11월 스터디/baekjoon11066.py
+++ 11월 스터디/baekjoon11066.py
@@ -1,1 +1,48 @@
-# 추후 풀이 예정
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    files = list(map(int, input().split()))
+
+    # subtotal[i] = files[0]부터 files[i-1]까지의 합
+    subtotal = [0] * (N + 1)
+    for i in range(1, N + 1):
+        subtotal[i] = subtotal[i - 1] + files[i - 1]
+
+    # dp[i][j]: i번째 파일부터 j번째 파일까지 합치는 최소 비용
+    dp = [[float('inf')] * N for _ in range(N)]
+
+    # opt_k[i][j]: dp[i][j]를 최소로 만드는 분할 지점 k
+    opt_k = [[0] * N for _ in range(N)]
+
+    # 길이가 1인 경우
+    for i in range(N):
+        dp[i][i] = 0
+        opt_k[i][i] = i
+
+    # 합치는 파일의 길이 (2부터 N까지)
+    for length in range(2, N + 1):
+        # j: 시작 파일 인덱스
+        for j in range(N - length + 1):
+            # end: 끝 파일 인덱스
+            end = j + length - 1
+
+            # 현재 구간 [j, end]를 합치는 비용 (마지막에 한 번 더해짐)
+            cost = subtotal[end + 1] - subtotal[j]
+
+            k_start = opt_k[j][end - 1]
+            k_end = opt_k[j + 1][end]
+
+            # k는 [j, end-1] 범위를 넘을 수 없음
+            for k in range(k_start, min(end, k_end + 1)):
+                current_cost = dp[j][k] + dp[k + 1][end]
+
+                if current_cost < dp[j][end]:
+                    dp[j][end] = current_cost
+                    opt_k[j][end] = k
+
+            dp[j][end] += cost
+
+    print(dp[0][N - 1])
--- 11월 스터디/baekjoon2304.py
+++ 11월 스터디/baekjoon2304.py
@@ -1,1 +1,50 @@
-# 추후 풀이 예정
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+rectangles = [list(map(int, input().split())) for _ in range(N)]
+
+rectangles.sort()
+
+# 가장 높은 직사각형의 높이 찾기
+max_height = 0
+for i in range(N):
+    max_height = max(max_height, rectangles[i][1])
+
+# 가장 높은 직사각형의 인덱스 찾기
+max_idx_list = []
+for i in range(N):
+    if rectangles[i][1] == max_height:
+        max_idx_list.append(i)
+
+# 가장 높은 직사각형의 인덱스가 여러개이면 첫 인덱스와 마지막 인덱스 사이는 전부 최대 높이로 지정
+first_max_idx = max_idx_list[0]
+last_max_idx = max_idx_list[-1]
+
+total_area = 0
+
+# 가장 높은 직사각형의 왼쪽 부분 영역 더하는 과정
+max_left = rectangles[0]
+for i in range(1, first_max_idx + 1):  # 첫 번째 최대 높이 기둥 *포함*
+    current_rectangle = rectangles[i]
+
+    # 현재 직사각형이 왼쪽 최대 높이보다 높거나 같으면
+    if current_rectangle[1] >= max_left[1]:
+        # 영역 더해주기
+        total_area += max_left[1] * (current_rectangle[0] - max_left[0])
+        # 최대 높이 갱신
+        max_left = current_rectangle
+
+# 오른쪽 부분 영역 더하는 과정
+max_right = rectangles[N - 1]
+for i in range(N - 2, last_max_idx - 1, -1):
+    current_rectangle = rectangles[i]
+
+    if current_rectangle[1] >= max_right[1]:
+        total_area += max_right[1] * (max_right[0] - current_rectangle[0])
+        max_right = current_rectangle
+
+# 최대 높이 부분 영역 더하기
+total_area += max_height * (rectangles[last_max_idx][0] - rectangles[first_max_idx][0] + 1)
+
+print(total_area)
